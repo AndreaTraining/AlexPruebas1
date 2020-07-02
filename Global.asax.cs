b906425e47d2833aac4f4282462dad7a6a51134c
CesarBetancurthR <67666653+CesarBetancurthR@users.noreply.github.com>
2020-07-01 19:44:07 -0500

    Update Global.asax.cs

--- Global.asax.cs
+++ Global.asax.cs
@@ -12,10 +12,16 @@ namespace QueryConsulta
     {
         protected void Application_Start()
         {
+            
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+        
+        public void Application_Start2()
+        {
+          //todo
         }
     }
 }
